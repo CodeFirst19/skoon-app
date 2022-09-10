5533a5bc80354a036ff228567bd45bb89dc02829
Kamogelo Mokgwatjane <kamogelomokgwatjane@sparksmiths.com>
2022-09-10 12:15:56 +0200

    Deployment preparation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://keepitskoon-app.herokuapp.com/api/v1',
+  //pk_key: 'pk_test_4a7d011b31248be9d5bd84e9b11ab193de825c02',
+  pk_key: 'pk_test_ac673bed1f8a386fb89ec0deebd31fc5906acec9',
 };
